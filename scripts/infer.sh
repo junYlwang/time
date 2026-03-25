ed92b6b8efa9e38fe54831ee2ea4ba9954f9e957
22wjy <junyi-wa22@mails.tsinghua.edu.cn>
2026-03-25 20:33:35 +0800

    add infer

---
+++ scripts/infer.sh
@@ -0,0 +1,24 @@
+GPU=1
+CPU=$((GPU * 8))
+MEMORY=$((GPU * 160000))
+rjob delete time-codec-infer
+rjob submit \
+  --name=time-codec-infer \
+  --gpu=$GPU \
+  --cpu=$CPU \
+  --memory=$MEMORY \
+  --charged-group="brainllm_gpu" \
+  --private-machine=group \
+  --mount=gpfs://gpfs1/brainllm-share:/mnt/shared-storage-user/brainllm-share \
+  --mount=gpfs://gpfs2/speechllm-share:/mnt/shared-storage-gpfs2/speechllm-share \
+  --mount=gpfs://gpfs2/brainllm2-share:/mnt/shared-storage-gpfs2/brainllm2-share \
+  --image=registry.h.pjlab.org.cn/ailab-brainllm-brainllm_gpu/junyi-workspace:wangjunyi-new \
+  --host-network=false \
+  -- bash -exc '
+set -ex
+. /root/miniconda3/etc/profile.d/conda.sh
+cd /mnt/shared-storage-gpfs2/brainllm2-share/junyi/time
+conda activate time
+python -m tools.infer \
+  --config /mnt/shared-storage-gpfs2/brainllm2-share/junyi/time/configs/time-codec-wo-quantize.yaml
+'
--- scripts/train.sh
+++ scripts/train.sh
@@ -1,9 +1,9 @@
 GPU=1
 CPU=$((GPU * 8))
 MEMORY=$((GPU * 160000))
-rjob delete time-codec-1000
+rjob delete time-codec-rfsq-1000
 rjob submit \
-  --name=time-codec-1000 \
+  --name=time-codec-rfsq-1000 \
   --gpu=$GPU \
   --cpu=$CPU \
   --memory=$MEMORY \
@@ -12,13 +12,13 @@ rjob submit \
   --mount=gpfs://gpfs1/brainllm-share:/mnt/shared-storage-user/brainllm-share \
   --mount=gpfs://gpfs2/speechllm-share:/mnt/shared-storage-gpfs2/speechllm-share \
   --mount=gpfs://gpfs2/brainllm2-share:/mnt/shared-storage-gpfs2/brainllm2-share \
-  --image=registry.h.pjlab.org.cn/ailab-brainllm-brainllm_gpu/junyi-workspace:wangjunyi-20260121133940 \
+  --image=registry.h.pjlab.org.cn/ailab-brainllm-brainllm_gpu/junyi-workspace:wangjunyi-new \
   --host-network=false \
   -- bash -exc '
 set -ex
 . /root/miniconda3/etc/profile.d/conda.sh
-cd /mnt/shared-storage-gpfs2/brainllm2-share/junyi/llm_assess
-conda activate codec
-torchrun --nproc_per_node=4 train.py \
---config /mnt/shared-storage-gpfs2/brainllm2-share/junyi/llm_assess/config.yaml
+cd /mnt/shared-storage-gpfs2/brainllm2-share/junyi/time
+conda activate time
+python -m tools.train \
+--config /mnt/shared-storage-gpfs2/brainllm2-share/junyi/time/configs/time-codec-rfsq-1000.yaml
 '
